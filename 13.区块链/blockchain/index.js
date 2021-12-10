5b155627633d16b85179301eef85e7c610850371
ty6666 <451376882@qq.com>
2021-12-10 09:40:31 +0800

    提交

--- 13.区块链/blockchain/index.js
+++ 13.区块链/blockchain/index.js
@@ -3,7 +3,12 @@
 * @author : 田源
 * @date : 2021-12-07 15:20
 * @description : 简易版本区块链
-*
+* 是一个链表
+* 由一个个Block组成
+* Block包含当前hash、上一个节点的hash、算力（计算难度）、时间戳、任意数据
+* 算力 是代表当前Block的挖掘次数，也是计算hash时的动态值，demo中要求挖出两个0的hash值即可（比特币要求挖10个0）
+* 安全性，由于每个Block中都包含上一个Block的hash值，所以，想要修改任意一个块的hash值，就需要修改他前面所有的block,这在显示世界中是无法实现的
+* isValid 用来判断Block的合法性
 */
 
 const hash = require("crypto-js/sha256")
