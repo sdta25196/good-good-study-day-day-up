cbcc97b606dadc1c3d00c7cced97cf623aa2492f
sdta25196 <451376882@qq.com>
2021-12-12 08:36:33 +0800

    提交

--- 04.webFarmework/z-1.工具集/ocr/readme.md
+++
@@ -1,9 +0,0 @@
-## 飞浆OCCR
-  [飞浆 github](https://github.com/PaddlePaddle/PaddleOCR/blob/release/2.3/README_ch.md)
-
-  
-
-
-
-
-
--- 15.py/test.py
+++ 15.py/test.py
@@ -1,42 +1,56 @@
-print("Hello, World!")
+# print("Hello, World!")
 
-print ("你好，世界")
+# print ("你好，世界")
 
-# raw_input("按下 enter 键退出，其他任意键显示...\n")
-import sys;
-x = 'runoob'
-sys.stdout.write(x + '\n')
+# # raw_input("按下 enter 键退出，其他任意键显示...\n")
+# import sys;
+# x = 'runoob'
+# sys.stdout.write(x + '\n')
 
-counter = 100 # 赋值整型变量
-miles = 1000.0 # 浮点型
-name = "John" # 字符串
+# counter = 100 # 赋值整型变量
+# miles = 1000.0 # 浮点型
+# name = "John" # 字符串
  
-print(counter)
-print(miles)
-print(name)
+# print(counter)
+# print(miles)
+# print(name)
 
 
-num = 1
-while num < 11:
-  num += 1
-  if(num % 2):
-    print(num)
-print(2)
+# num = 1
+# while num < 11:
+#   num += 1
+#   if(num % 2):
+#     print(num)
+# print(2)
 
 
-import time  # 引入time模块
+# import time  # 引入time模块
  
-ticks = time.time()
-print("当前时间戳为:", ticks)
+# ticks = time.time()
+# print("当前时间戳为:", ticks)
 
 
-def d(str):
-  print(str)
-  return 999
+# def d(str):
+#   print(str)
+#   return 999
 
-l = d(666) 
-print(l)
+# l = d(666) 
+# print(l)
 
-f = lambda a,b: a + b
+# f = lambda a,b: a + b
 
-print(f(2,3))
+# print(f(2,3))
+
+
+# l1 = [1,2,True,3,'sss',('a',2)]
+# print(l1 * 3)
+# print(l1)
+
+# for x in l1[:]:
+#   print(x)
+  
+
+line = [1,2,3]
+print(type(str(line)))
+
+print(str(2.0))
