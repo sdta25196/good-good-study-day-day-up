08cbee72b6bd2a41137d976e75b53ac169517354
ty6666 <451376882@qq.com>
2021-12-30 16:55:52 +0800

    提交

--- 04.webFarmework/z-2.coding/01-02.防抖节流/go/src/debounce.ts
+++ 04.webFarmework/z-2.coding/01-02.防抖节流/go/src/debounce.ts
@@ -1,3 +1,4 @@
+// 最后一次触发说了算
 // 防抖，单位时间内，只要触发就重新计算执行时间, 一直到不触发执行了，才会真正执行一次
 export function debounce(fn: Function, delay: number = 300) {
   let I = null;
--- 04.webFarmework/z-2.coding/01-02.防抖节流/go/src/throttle.ts
+++ 04.webFarmework/z-2.coding/01-02.防抖节流/go/src/throttle.ts
@@ -1,4 +1,5 @@
-// 节流, 每个单位时间内触发上锁，单位时间只会执行一次
+// 第一次触发说了算，只要进入就上锁
+// 节流, 每个单位时间内触发上锁，单位时间只会执行一次 
 export function throttle(fn: Function, interval: number = 300) {
   let lock = false;
   let start: number;
@@ -17,3 +18,27 @@ export function throttle(fn: Function, interval: number = 300) {
     }, interval - ellapsed % interval)
   }
 }
+
+
+
+// function throttle1(fn, t) {
+//   let lock = false
+//   return (...params) => {
+//     if (lock) return
+//     lock = true
+//     fn(...params)
+//     setTimeout(() => {
+//       lock = false
+//     }, t)
+//   }
+// }
+
+// function debounce(fn, t) {
+//   let timer
+//   return (...params) => {
+//     if (timer) clearTimeout(timer)
+//     timer = setTimeout(() => {
+//       fn(...params)
+//     }, t)
+//   }
+// }
