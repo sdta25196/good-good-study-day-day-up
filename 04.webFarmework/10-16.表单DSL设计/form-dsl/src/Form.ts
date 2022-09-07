0b170a8d1efc7d5f8181c9a9bfe31acb94105005
sdta25196 <451376882@qq.com>
2022-09-07 23:12:26 +0800

    t

--- 04.webFarmework/10-16.表单DSL设计/form-dsl/src/Form.ts
+++ 04.webFarmework/10-16.表单DSL设计/form-dsl/src/Form.ts
@@ -23,9 +23,10 @@ export class FormItem {
   }
 
   public getValue() {
-    // const val = this.form.getValue(this.meta.path!)
+    const val = this.form.getValue(this.meta.path!)
     // if (typeof val === 'undefined') {
-    return this.meta.default
+    return val
+    // return this.meta.default
     // }
   }
   public setValue(val: any) {
