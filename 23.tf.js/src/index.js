31f8db9ff3772df8cf97696dc814b06306ba17d7
sdta25196 <451376882@qq.com>
2023-04-25 14:22:47 +0800

    feat: learn

--- 23.tf.js/src/index.js
+++ 23.tf.js/src/index.js
@@ -38,4 +38,24 @@ async function run() {
   // More code will be added below
 }
 
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
+
+  // Add a single input layer
+  model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
+
+  // Add an output layer
+  model.add(tf.layers.dense({ units: 1, useBias: true }));
+
+  return model;
+}
+
+// Create the model
+const model = createModel();
+
+tfvis.show.modelSummary({ name: 'Model Summary' }, model);
+
 document.addEventListener('DOMContentLoaded', run);
+
+
