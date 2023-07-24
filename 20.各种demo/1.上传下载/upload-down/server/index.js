abd24ef8bffc5f6692f777a0f7868f9360b4b1b0
sdta25196 <451376882@qq.com>
2023-07-24 16:51:56 +0800

    feat: 新增上传下载

---
+++ 20.各种demo/1.上传下载/upload-down/server/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+var multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+var cors = require('cors')
+const app = express()
+
+const clientPath = path.resolve(__dirname, '../client/')
+const staticPath = path.resolve(__dirname, '../client/static/')
+
+app.use(require('body-parser').urlencoded({ extended: false }))
+app.use(cors())
+app.use(express.static(clientPath))
+
+// 文件上传
+app.post('/upload', multer({ dest: staticPath }).any(), function (req, res) {
+    let file = req.files[0]
+    // 拼装文件名称
+    let filename = path.resolve(staticPath, req.body.fileName)
+    // 为上传成功的文件重命名（上传的文件默认不是文件的原名称）
+    fs.rename(file.path, filename, function (err) {
+        if (err) {
+            res.send({ code: 1, message: err, data: "" })
+        } else {
+            res.send({ code: 0, message: "成功", data: filename })
+        }
+    })
+})
+
+// 文件下载
+app.get('/download', function (req, res) {
+    // 如果参数中有中文，需要解码
+    // 判断该文件是否存在
+    const filePath = path.resolve(staticPath, '2.png')
+    fs.access(filePath, function (err) {
+        if (!err) {
+            res.set({
+                // 告诉浏览器这是一个二进制文件 、 也可以使用明确的文件类型
+                "Content-Type": "application/octet-stream",
+                // attachment 是告诉浏览器这是一个需要下载的文件，
+                // filename 是建议的文件命名，使用encodeURI方法，是为了避免中文名称下载时出问题
+                "Content-Disposition": encodeURI(`attachment;filename=2.png`)
+            })
+            // 使用流读取文件，并响应给客户端
+            fs.createReadStream(filePath).pipe(res)
+        }
+    })
+
+})
+
+
+app.get('*', (req, res) => {
+    let str = fs.readFileSync(path.resolve(clientPath, "index.html"), 'utf-8')
+    res.send(str)
+})
+
+const port = process.env.PORT || 9997
+
+app.listen(port, () => {
+    console.log(`listen http://localhost:${port}`)
+})
