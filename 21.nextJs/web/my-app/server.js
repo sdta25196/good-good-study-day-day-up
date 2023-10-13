a1127fbf43916dc931c6e94c738d179528bad49f
sdta25196 <451376882@qq.com>
2023-10-13 14:24:09 +0800

    feat: 新增docker-nextjs

--- 21.nextJs/web/my-app/server.js
+++ 21.nextJs/web/my-app/server.js
@@ -1,2 +1,18 @@
 const cmd=require('node-cmd'); 
 cmd.run('yarn start');
+
+// const express = require('express')
+// const next = require('next');
+// const dev = true
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   const server = express();
+//   server.get('*', (req, res) => {
+//     return handle(req, res)
+//   })
+//   server.listen(3000, (err) => {
+//     console.log('> ready on http://localhost:3000')
+//   })
+// })
