81fd80037f35a0d8c94859a9798df65a82fe3792
sdta25196 <451376882@qq.com>
2023-12-06 17:46:27 +0800

    feat: 新增生成新单词的功能

--- 24.灵境/单词本/api/lingjing.js
+++ 24.灵境/单词本/api/lingjing.js
@@ -22,7 +22,7 @@ let wordbook = [
 ]
 
 app.all('/get_wordbook', (req, res) => {
-  log('展示单词本', req.get('Authorization'))
+  log('展示单词本，' + req.get('Authorization'))
   res.send({
     wordbook: wordbook,
     prompt: "使用markdown语法，以表格的形式显示所有单词"
@@ -49,6 +49,47 @@ app.all('/delete_word', (req, res) => {
   })
 })
 
+
+app.get('/generate_word', function (req, res) {
+  log('生成新单词，' + req.get('Authorization'))
+  // 设置HTTP响应头信息
+  // 设置MIME类型为event-stream
+  res.setHeader('Content-Type', 'text/event-stream');
+  // 设置连接方式为keep-alive
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  let word = "xioswa"
+  let wordArray = word.split("")
+  let sendEndWord = ""
+  let i = 0
+
+  function sendEvents() {
+    // 定义事件数据
+    const data = {
+      word: sendEndWord + wordArray[i],
+      prompt: "请基于工具返回的word字段，提示给用户新生成的单词"
+    };
+
+    sendEndWord = data.word;
+    i++;
+
+    if (sendEndWord == word) {
+      clearInterval(timer); // 清除定时器
+      res.end(); // 关闭连接
+    } else {
+      // 使用SSE格式发送事件数据
+      // "data: "为固定前缀，后接JSON格式的事件数据
+      res.write(`data: ${JSON.stringify(data)}\n\n`);
+    }
+  }
+
+  // 每秒发送一次事件
+  let timer = setInterval(sendEvents, 1000);
+});
+
+
+// ! SSE 示例
 app.get('/stream', function (req, res) {
   // 设置HTTP响应头信息
   // 设置MIME类型为event-stream
@@ -66,13 +107,13 @@ app.get('/stream', function (req, res) {
     // 使用SSE格式发送事件数据
     // "data: "为固定前缀，后接JSON格式的事件数据
     res.write(`data: ${JSON.stringify(data)}\n\n`);
-
-    // 收到请求关闭连接的情况处理
-    req.on('close', () => {
-      clearInterval(timer); // 清除定时器
-      res.end(); // 关闭连接
-    });
   }
+
+  // 收到请求关闭连接的情况处理
+  req.on('close', () => {
+    clearInterval(timer); // 清除定时器
+    res.end(); // 关闭连接
+  });
 
   // 每秒发送一次事件
   let timer = setInterval(sendEvents, 1000);
