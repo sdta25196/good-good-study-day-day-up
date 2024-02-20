296bbc7202c5ca2ddc76cc3d652ba6de656a8abe
sdta25196 <451376882@qq.com>
2024-02-20 17:30:26 +0800

    chore: none

---
+++ 23.LLM/py/hello.py
@@ -0,0 +1,3 @@
+# hello  模块
+def sayHello():
+    print("Hello")
---
+++ 23.LLM/py/test.py
@@ -0,0 +1,18 @@
+a = 1
+
+print(a)
+
+c=(1,2,34)
+
+for d in c:
+    print(d)
+
+class S:
+    def s():
+        print(a)
+
+S.s()
+
+
+
+
